8125f4268e200c5cabc55eb61f38e256d9f0c1a4
pmmson <pmmson@gmail.com>
2019-11-11 06:35:00 +0300

    Add MVC infrastructure in Project

---
+++ WebStore/WebStore/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly List<EmployeeView> _employees = new List<EmployeeView>
+        {
+            new EmployeeView
+            {
+                Id = 1,
+                FirstName = "Иван",
+                SurName = "Иванов",
+                Patronymic = "Иванович",
+                Age = 22
+            },
+            new EmployeeView
+            {
+                Id = 2,
+                FirstName = "Владислав",
+                SurName = "Петров",
+                Patronymic = "Иванович",
+                Age = 35
+            }
+        };
+
+        public IActionResult Index()
+        {
+            return View(_employees);
+        }
+    }
+}
