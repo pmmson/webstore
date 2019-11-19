a509afecd817596fddae1c1b41b4fd5c37db3890
pmmson <pmmson@gmail.com>
2019-11-19 11:19:18 +0300

    Добавление методов редактирования и удаления сотрудников из списка

--- WebStore/WebStore/Controllers/EmployeesController.cs
+++ WebStore/WebStore/Controllers/EmployeesController.cs
@@ -29,5 +29,55 @@ namespace WebStore.Controllers
 
             return View(employee);
         }
+
+        [Route("edit/{id?}")]
+        public IActionResult Edit(int? id)
+        {
+            EmployeeView model;
+            if (id.HasValue)
+            {
+                model = _employeesData.GetById(id.Value);
+                if (ReferenceEquals(model, null))
+                    return View("Error404");
+            }
+            else
+            {
+                model = new EmployeeView();
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [Route("edit/{id?}")]
+        public IActionResult Edit(EmployeeView model)
+        {
+            if(model.Id > 0)
+            {
+                var dbItem = _employeesData.GetById(model.Id);
+
+                if (ReferenceEquals(model, null))
+                    return View("Error404");
+
+                dbItem.FirstName = model.FirstName;
+                dbItem.SurName = model.SurName;
+                dbItem.Patronymic = model.Patronymic;
+                dbItem.Age = model.Age;
+                dbItem.Position = model.Position;
+            }
+            else
+            {
+                _employeesData.AddNew(model);
+            }
+            _employeesData.Commit();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [Route("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            _employeesData.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
