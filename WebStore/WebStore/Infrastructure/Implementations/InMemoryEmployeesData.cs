2b599d4357975bb5349ddbef75f04b3b3306512f
pmmson <pmmson@gmail.com>
2019-11-19 07:23:00 +0300

    Перенос данных в сервис, добавление маршрутизации через атрибуты

---
+++ WebStore/WebStore/Infrastructure/Implementations/InMemoryEmployeesData.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+
+namespace WebStore.Infrastructure.Implementations
+{
+    public class InMemoryEmployeesData : IEmployeesData
+    {
+        private readonly List<EmployeeView> _employees;
+
+        public InMemoryEmployeesData()
+        {
+            _employees = new List<EmployeeView>(3)
+            {
+                new EmployeeView()
+                {
+                    Id = 1,
+                    FirstName = "Иван",
+                    SurName = "Иванов",
+                    Patronymic = "Иванович",
+                    Age = 22,
+                    Position = "консультант"
+                },
+                new EmployeeView()
+                {
+                    Id = 2,
+                    FirstName = "Владислав",
+                    SurName = "Петров",
+                    Patronymic = "Иванович",
+                    Age = 35,
+                    Position = "товаровед"
+                },
+                new EmployeeView()
+                {
+                    Id = 3,
+                    FirstName = "Александр",
+                    SurName = "Сидоров",
+                    Patronymic = "Викторович",
+                    Age = 39,
+                    Position = "менеджер"
+                }
+            };
+        }
+
+        public void AddNew(EmployeeView model)
+        {
+            model.Id = _employees.Max(e => e.Id) + 1;
+            _employees.Add(model);
+        }
+
+        public void Commit()
+        {
+            // ...
+        }
+
+        public void Delete(int id)
+        {
+            var employee = GetById(id);
+            if (employee != null)
+                _employees.Remove(employee);
+        }
+
+        public IEnumerable<EmployeeView> GetAll()
+        {
+            return _employees;
+        }
+
+        public EmployeeView GetById(int id)
+        {
+            return _employees.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
