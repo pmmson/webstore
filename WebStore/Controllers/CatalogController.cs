11185c5c7caa0701591f4e7bb7995be888c874ea
pmmson <pmmson@gmail.com>
2019-11-23 18:48:37 +0300

    изменение контроллера для представления Shop

--- WebStore/Controllers/CatalogController.cs
+++ WebStore/Controllers/CatalogController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models.Product;
 
 namespace WebStore.Controllers
 {
@@ -10,6 +13,30 @@ namespace WebStore.Controllers
     {
         public IActionResult ProductDetails() => View();
 
-        public IActionResult Shop() => View();
+        private readonly IProductData _productData;
+
+        public CatalogController(IProductData productData) => _productData = productData;
+
+        public IActionResult Shop(int? sectionId, int? brandId)
+        {
+            var products = _productData.GetProducts(new ProductFilter { BrandId = brandId, SectionId = sectionId });
+
+            var model = new CatalogViewModel()
+            {
+                BrandId = brandId,
+                SectionId = sectionId,
+                Products = products.Select(p => new ProductViewModel()
+                {
+                    Id = p.Id,
+                    ImageUrl = p.ImageUrl,
+                    Name = p.Name,
+                    Order = p.Order,
+                    Price = p.Price
+                }).OrderBy(p => p.Order).ToList()
+            };
+
+            return View(model);
+
+        }
     }
 }
