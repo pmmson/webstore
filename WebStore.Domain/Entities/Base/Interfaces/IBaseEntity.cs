72dd74f407ceaa9de18b2aaa4951b3ef32c2ca82
pmmson <pmmson@gmail.com>
2019-11-23 09:09:09 +0300

    создание структуры сцщности Section и Brand

---
+++ WebStore.Domain/Entities/Base/Interfaces/IBaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.Domain.Entities.Base.Interfaces
+{
+    interface IBaseEntity
+    {
+        int Id { get; set; }
+    }
+}
---
+++ WebStore.Domain/Entities/Base/Interfaces/IOrderedEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.Domain.Entities.Base.Interfaces
+{
+    interface IOrderedEntity
+    {
+        int Order { get; set; }
+    }
+}
