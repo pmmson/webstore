72fd83f80ef23363b422780b27c398f3fc970b57
pmmson <pmmson@gmail.com>
2019-11-23 18:52:23 +0300

    реализация сущности Товары

--- WebStore/Infrastructure/Interfaces/IProductData.cs
+++ WebStore/Infrastructure/Interfaces/IProductData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Domain.Entities;
+using WebStore.Models.Product;
 
 namespace WebStore.Infrastructure.Interfaces
 {
@@ -10,5 +11,6 @@ namespace WebStore.Infrastructure.Interfaces
     {
         IEnumerable<Section> GetSections();
         IEnumerable<Brand> GetBrands();
+        IEnumerable<Product> GetProducts(ProductFilter filter);
     }
 }
---
+++ WebStore/Models/Product/ProductViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Models.Product
+{
+    public class ProductViewModel : INamedEntity, IOrderedEntity
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int Order { get; set; }
+
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
