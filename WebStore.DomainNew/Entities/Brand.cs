93734f60aaf3c6f69ba48900dc59a8d5224bd854
pmmson <pmmson@gmail.com>
2019-11-28 21:08:05 +0300

    создание проекта для переноса сервиса в БД и маппинг сущностей на будущие таблицы БД

---
+++ WebStore.DomainNew/Entities/Brand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities
+{
+    [Table("Brands")]
+    public class Brand : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+    }
+}
---
+++ WebStore.DomainNew/Entities/Section.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities
+{
+    [Table("Sections")]
+    public class Section : NamedEntity, IOrderedEntity
+    {
+        public int? ParentId { get; set; }
+
+        [ForeignKey("ParentId")]
+        public virtual Section ParentSection { get; set; }
+
+        public int Order { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
