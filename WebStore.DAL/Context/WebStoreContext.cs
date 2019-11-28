752f46a627042e72b9cf5572b0924b71ee2575de
pmmson <pmmson@gmail.com>
2019-11-28 21:31:34 +0300

    создание проекта для хранения логики БД

---
+++ WebStore.DAL/Context/WebStoreContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebStore.DomainNew.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebStore.DAL.Context
+{
+    public class WebStoreContext : DbContext
+    {
+        public WebStoreContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+
+    }
+}
