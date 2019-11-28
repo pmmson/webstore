cea96eb661a572891c213d0a24861849245f2448
pmmson <pmmson@gmail.com>
2019-11-28 16:11:31 +0300

    загрузка товаров на основную страницу из сервиса

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities;
+using WebStore.Infrastructure.Interfaces;
 using WebStore.Models;
+using WebStore.Models.Product;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => View();
-
+        public IActionResult Index([FromServices] IProductData _productData)
+        {
+            return View(new CatalogViewModel
+            {
+                Products = _productData.GetProducts(new ProductFilter()).Select(p => new ProductViewModel
+                {
+                    Id = p.Id,
+                    ImageUrl = p.ImageUrl,
+                    Name = p.Name,
+                    Order = p.Order,
+                    Price = p.Price
+                })
+            });
+        }
         public IActionResult Blog() => View();
 
         public IActionResult BlogSingle() => View();
