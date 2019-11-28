d2eb1a1ad77a59c8f10ba65e33564f0f24e8084d
pmmson <pmmson@gmail.com>
2019-11-28 22:48:52 +0300

    работа с полем типа decimal

--- WebStore.DomainNew/Entities/Product.cs
+++ WebStore.DomainNew/Entities/Product.cs
@@ -24,6 +24,7 @@ namespace WebStore.Domain.Entities
 
         public string ImageUrl { get; set; }
 
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
 
     }
