dc40d0736f3f801c947760a27ca73ba2ed076245
pmmson <pmmson@gmail.com>
2019-11-30 11:16:15 +0300

    добавлены атрибуты для отображения имен переменных на представлении

--- WebStore/Models/Account/RegisterUserViewModel.cs
+++ WebStore/Models/Account/RegisterUserViewModel.cs
@@ -8,14 +8,20 @@ namespace WebStore.Models.Account
 {
     public class RegisterUserViewModel
     {
+        [Display(Name = "Имя пользователя")] //TODO: не работает отображение на форме имени
         [Required, MaxLength(256)]
         public string UserName { get; set; }
 
+        [Display(Name = "Пароль")]
         [Required, DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [Display(Name = "Подтверждение пароля")]
         [DataType(DataType.Password), Compare(nameof(Password))]
         public string ConfirmPassword { get; set; }
+
+        [Display(Name = "E-mail пользователя")]
+        public string UserEmailAddress { get; set; }
 
     }
 }
