2ce654adaf8ff176701cc0241df868b29b5307f9
pmmson <pmmson@gmail.com>
2019-12-05 05:06:01 +0300

    подключение Identity к проекту и миграция в БД инфраструктуры

--- WebStore/Migrations/20191205020411_identity.Designer.cs
+++ WebStore/Migrations/20191205020411_identity.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebStore.DAL.Context;
 
 namespace WebStore.Migrations
 {
     [DbContext(typeof(WebStoreContext))]
-    partial class WebStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20191205020411_identity")]
+    partial class identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
