deeddd6f53dade9b377a71fe32551b671e52f6ff
pmmson <pmmson@gmail.com>
2019-12-05 20:58:35 +0300

    создание модели корзины

---
+++ WebStore/Models/Cart/Cart.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebStore.Models.Cart
+{
+    public class Cart
+    {
+        public List<CartItem> Items { get; set; }
+
+        public int ItemsCount => Items?.Sum(x => x.Quantity) ?? 0;
+    }
+}
---
+++ WebStore/Models/Cart/CartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebStore.Models.Product;
+
+namespace WebStore.Models.Cart
+{
+    public class CartViewModel
+    {
+        public Dictionary<ProductViewModel, int> Items { get; set; }
+
+        public int ItemsCount => Items?.Sum(x => x.Value) ?? 0;
+    }
+}
