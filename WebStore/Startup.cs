28f19534a260906116ebc065e4e6946b5f129711
pmmson <pmmson@gmail.com>
2019-12-07 06:22:18 +0300

    создание области Админки сайта

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -88,6 +88,10 @@ namespace WebStore
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
