5be8fb5b3d9b9a00a9c5e744f89e93dd174ebfe1
pmmson <pmmson@gmail.com>
2019-12-07 06:37:30 +0300

    создание сущностей для заказов

--- WebStore.DAL/Context/WebStoreContext.cs
+++ WebStore.DAL/Context/WebStoreContext.cs
@@ -16,5 +16,8 @@ namespace WebStore.DAL.Context
         public DbSet<Section> Sections { get; set; }
         public DbSet<Brand> Brands { get; set; }
 
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
     }
 }
--- WebStore/Migrations/20191207033501_orders.Designer.cs
+++ WebStore/Migrations/20191207033501_orders.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebStore.DAL.Context;
 
 namespace WebStore.Migrations
 {
     [DbContext(typeof(WebStoreContext))]
-    partial class WebStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20191207033501_orders")]
+    partial class orders
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
