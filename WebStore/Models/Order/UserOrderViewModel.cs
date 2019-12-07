d6d11f325f939301dc57af87a5c843220a95f987
pmmson <pmmson@gmail.com>
2019-12-07 09:02:45 +0300

    реализация просмотра заказов для зарегистрированного пользователя

---
+++ WebStore/Models/Order/UserOrderViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace WebStore.Models.Order
+{
+    public class UserOrderViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public decimal TotalSum { get; set; }
+
+    }
+}
